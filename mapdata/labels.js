111b4065c46477b591b7c5c031e4fc5d8720468c
Paul Catalan <contact.pcatl@gmail.com>
2019-11-21 20:56:45 +0800

    Finally fixed the bug that bombards the map with tons of displayed pathways

--- mapdata/labels.js
+++ mapdata/labels.js
@@ -1,130 +1,130 @@
 var Labels = {
-    "Labels":[
+    "Labels": [
         {
-            "name":"Parking Bldg.",
+            "name": "Parking Bldg.",
             "x": "270",
             "y": "100",
-            "for":"Parking_Lot, 52"// format: <name of building>, <indexnumber> in mapdata.json
+            "for": "Parking_Lot, 52"// format: <name of building>, <indexnumber> in mapdata.json
         },
         {
-            "name":"D. Silang",
+            "name": "D. Silang",
             "x": "235",
             "y": "330",
-            "for":"Silang, 53"
+            "for": "Silang, 53"
         },
         {
-            "name":"De Haes",
+            "name": "De Haes",
             "x": "223",
             "y": "155",
-            "for":"DH, 51"
+            "for": "DH, 51"
         },
         {
-            "name":"(P.E.)",
+            "name": "(P.E.)",
             "x": "240",
             "y": "175",
-            "for":"DH, 51"
+            "for": "DH, 51"
         },
         {
-            "name":"Chapel",
+            "name": "Chapel",
             "x": "295",
             "y": "210",
-            "for":"Chapel, 50"
+            "for": "Chapel, 50"
         },
         {
-            "name":"W. Perfecto",
+            "name": "W. Perfecto",
             "x": "360",
             "y": "260",
-            "for":"Perfecto, 55"
+            "for": "Perfecto, 55"
         },
         {
-            "name":"C. Adenaur",
+            "name": "C. Adenaur",
             "x": "459",
             "y": "462",
-            "for":"Aden, 54"
+            "for": "Aden, 54"
         },
         {
-            "name":"P.B. Gym",
+            "name": "P.B. Gym",
             "x": "135",
             "y": "591",
             "for": "PrinceBernhardGym, 59"
         },
         {
-            "name":"C. Vath",
+            "name": "C. Vath",
             "x": "265",
             "y": "586",
             "for": "Vath, 45"
         },
         {
-            "name":"(Library)",
+            "name": "(Library)",
             "x": "260",
             "y": "606",
             "for": "Vath, 45"
         },
         {
-            "name":"J.P. Rizal",
+            "name": "J.P. Rizal",
             "x": "397",
             "y": "590",
-            "for":"Rizal, 49"
+            "for": "Rizal, 49"
         },
         {
-            "name":"CCA",
+            "name": "CCA",
             "x": "135",
             "y": "731",
-            "for":"CCA, 56"
+            "for": "CCA, 56"
         },
         {
-            "name":"Annex",
+            "name": "Annex",
             "x": "162",
             "y": "805",
-            "for":"Annex, 48"
+            "for": "Annex, 48"
         },
         {
-            "name":"O. Hahn",
+            "name": "O. Hahn",
             "x": "500",
             "y": "695",
-            "for":"Hahn, 57"
+            "for": "Hahn, 57"
         },
         {
-            "name":"Hospital",
+            "name": "Hospital",
             "x": "320",
             "y": "786",
-            "for":""
+            "for": "SacredHeart, 46"
         },
         {
-            "name":"(Sacred Heart)",
+            "name": "(Sacred Heart)",
             "x": "310",
             "y": "802",
-            "for":"SacredHeart, 46"
+            "for": "SacredHeart, 46"
         },
         {
-            "name":"H.",
+            "name": "H.",
             "x": "491",
             "y": "838",
-            "for":"DB, 63"
+            "for": "DB, 63"
         },
         {
-            "name":"Del Baere",
+            "name": "Del Baere",
             "x": "473",
             "y": "855",
-            "for":"DB, 63"
+            "for": "DB, 63"
         },
         {
-            "name":"Mount Mary",
+            "name": "Mount Mary",
             "x": "580",
             "y": "900",
-            "for":"MaryMount, 60"
+            "for": "MaryMount, 60"
         },
         {
-            "name":"Student Council",
+            "name": "Student Council",
             "x": "335",
             "y": "1125",
-            "for":"StudentCouncil, 61"
+            "for": "StudentCouncil, 61"
         },
         {
-            "name":"& Men's Dormitory",
+            "name": "& Men's Dormitory",
             "x": "345",
             "y": "1145",
-            "for":"MensDorm, 39"
+            "for": "MensDorm, 39"
         }
     ]
 };
